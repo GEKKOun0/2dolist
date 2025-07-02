cd4685e35554800cbe0f2f6fd77441816b703a89
unknown <gekkoone7@gmail.com>
2025-07-01 20:26:16 -0600

    Añadiendo el formulario de creacion de tareas

--- js/tasks.js
+++ js/tasks.js
@@ -1,7 +1,20 @@
+modal-container
+taskForm
+addTaskBtn
+taskCreateBtn
 
+const addTaskBtn = document.querySelector('#addTaskBtn');
+const taskCreateBtn = document.querySelector('#taskCreateBtn');
+const taskForm = document.querySelector('taskForm');
+const modal = document.querySelector('modal');
 
+addTaskBtn.addEventListener('click', () => {
+    modal.classList.add('show');
+});
 
-
+taskCreateBtn.addEventListener('click', () => {
+    modal.classList.remove('show');
+});
 
 
 
